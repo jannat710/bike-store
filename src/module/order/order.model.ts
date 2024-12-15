13bf24c08dded82c11468d421db3e7e6d3a3c81b
e <jannatul0040@gmail.com>
2024-12-15 22:36:30 +0600

    modified in order.model.ts file

--- src/module/order/order.model.ts
+++ src/module/order/order.model.ts
@@ -21,10 +21,24 @@ const orderSchema = new Schema({
   quantity: {
     type: Number,
     required: [true, 'Please provide a valid quantity'],
+    min: [1, 'Quantity must be at least 1'],
+    validate: {
+      validator: function (value: number) {
+        return Number.isInteger(value);
+      },
+      message: 'Quantity must be an integer',
+    },
   },
   totalPrice: {
     type: Number,
-    required: [true, 'Please provide a valid number'],
+    required: [true, 'Please provide a valid total price'],
+    min: [0, 'Total price must be a positive number'],
+    validate: {
+      validator: function (value: number) {
+        return value > 0;
+      },
+      message: 'Total price must be greater than 0',
+    },
   },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
